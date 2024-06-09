fa6a6bb1425c787108d8d7e3e3b1bbb585e2d0b1
RMorales25 <rj.m.m.2500@estudiantec.cr>
2024-06-08 18:31:52 -0600

    delete unnecessary files

--- modules/unification/compile_unification.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo 'Compiling *.c *cpp files'
-rm -rf unification.o
-#export SYSTEMC_HOME=/usr/local/systemc-3.0.0/
-#export LD_LIBRARY_PATH=$SYSTEMC_HOME/lib-linux64
-g++ -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib-linux64 unification_tb.cpp unification_pv_model.cpp  -lsystemc -lm -o unification.o
-echo 'Simulation Started'
-./unification.o
-echo 'Simulation Ended'
