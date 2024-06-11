6113375035f4987ad709bd5dd687d59bde396359
Cruz Soto, Federico A <federico.a.cruz.soto@intel.com>
2024-06-11 15:58:03 -0600

    arregla typo en script de at

--- modules/compression/compile_run_at.sh
+++ modules/compression/compile_run_at.sh
@@ -5,7 +5,7 @@ export SYSTEMC_HOME=/usr/local/systemc-2.3.3/
 export OPENCV_H_DIR=/usr/local/include/opencv4/
 export OPENCV_SO_DIR=/usr/local/lib/
 export LD_LIBRARY_PATH=$SYSTEMC_HOME/lib-linux64
-g++ -I$SYSTEMC_HOME/include -I$OPENCV_H_DIR -Iinclude/ -L$SYSTEMC_HOME/lib-linux64 -L$OPENCV_SO_DIR src/ips_jpg_at_testbench.hpp include/ips_jpg_at_model.cpp  -lsystemc -lm -o ips_jpg.o
+g++ -I$SYSTEMC_HOME/include -I$OPENCV_H_DIR -Iinclude/ -L$SYSTEMC_HOME/lib-linux64 -L$OPENCV_SO_DIR src/ips_jpg_at_testbench.cpp include/ips_jpg_at_model.hpp  -lsystemc -lm -o ips_jpg.o
 echo 'Simulation Started'
 ./ips_jpg.o
 echo 'Simulation Ended'
