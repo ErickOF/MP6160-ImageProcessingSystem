5ca3318ed05904bbec269e26847cb446cdbbf5c8
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2024-07-06 11:49:39 -0600

    Adding documentation for ADC module and its testbench

--- modules/adc/include/seq_item_adc.hpp
+++ modules/adc/include/seq_item_adc.hpp
@@ -1,16 +1,23 @@
-#ifndef IPS_TEST_ADC_HPP
-#define IPS_TEST_ADC_HPP
+#ifndef IPS_SEQ_ITEM_ADC_HPP
+#define IPS_SEQ_ITEM_ADC_HPP
 
 #include <cstdlib>
 
 
+/**
+ * @brief This class is used to generate the analog signal for the test
+ * 
+ * @tparam N 
+ */
 template <unsigned int N>
-SCA_TDF_MODULE(test_adc) {
+SCA_TDF_MODULE(seq_item_adc)
+{
 public:
   sca_tdf::sca_out<double> o_ana;
   const int MAX_CODE = (1 << N);
 
-  SCA_CTOR(test_adc) {
+  SCA_CTOR(seq_item_adc)
+  {
     set_timestep(sca_core::sca_time(0.1, sc_core::SC_US));
   }
 
@@ -20,4 +27,4 @@ public:
   }
 };
 
-#endif // IPS_TEST_ADC_HPP
+#endif // IPS_SEQ_ITEM_ADC_HPP
--- modules/adc/src/tb_adc.cpp
+++ modules/adc/src/tb_adc.cpp
@@ -1,29 +1,41 @@
 #include <systemc-ams.h>
 #include "adc.hpp"
-#include "test_adc.hpp"
+#include "seq_item_adc.hpp"
 
 #define N 8
 
 
 int sc_main(int, char*[])
 {
+  // Max number of sequence items to test
+  const int MAX_SEQ_ITEMS = (1 << N) - 1;
+
+  // Signals to connect
   sca_tdf::sca_signal<double> s_ana;
   sca_tdf::sca_signal<sc_dt::sc_uint<N> > s_dig_out;
 
-  const int MAX_ITER = (1 << N) - 1;
-
-  dac<N> ips_adc("ips_adc");
+  // DUT
+  adc<N> ips_adc("ips_adc");
   ips_adc.in(s_ana);
   ips_adc.out(s_dig_out);
 
-  test_adc<N> ips_test_adc("ips_test_adc");
-  ips_test_adc.o_ana(s_ana);
+  // Sequence item generator for ADC
+  seq_item_adc<N> ips_seq_item_adc("ips_seq_item_adc");
+  ips_seq_item_adc.o_ana(s_ana);
 
+  // Dump waveform
   sca_util::sca_trace_file* tf = sca_util::sca_create_vcd_trace_file("ips_adc");
   sca_util::sca_trace(tf, s_ana, "in");
   sca_util::sca_trace(tf, s_dig_out, "out");
 
-  sc_start(MAX_ITER * 0.1, SC_US);
+  // Start time
+  std::cout << "@" << sc_time_stamp() << std::endl;
+
+  // Run test
+  sc_start(MAX_SEQ_ITEMS * 0.1, SC_US);
+
+  // End time
+  std::cout << "@" << sc_time_stamp() << std::endl;
 
   sca_util::sca_close_vcd_trace_file(tf);
 
