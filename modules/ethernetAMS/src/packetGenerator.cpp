4c47020373c6c3b38a316db5628cd06148bd9348
fa-jimenez <najimenezch@gmail.com>
2024-07-17 19:03:56 -0600

    Change behavior of packet generator to use SCA_TDF_MODULE

--- modules/ethernetAMS/src/packetGenerator.cpp
+++ modules/ethernetAMS/src/packetGenerator.cpp
@@ -1,11 +1,15 @@
+#include <systemc-ams.h>
+#include <systemc.h>
 #include "packetGenerator.h"
 
-void packetGenerator::fill_data(unsigned char* data, int nibbles_length)
+
+void packetGenerator::fill_data(unsigned char* data, int packet_length)
 {
-  int actual_length = (nibbles_length > N) ? N : nibbles_length;
+  int actual_length = (packet_length * 2 > N) ? N : packet_length * 2;
   unsigned char tmp_data;
-  sc_dt::sc_bv<N * 4> tmp_data_in = data_in.read();
-  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid.read();
+  sc_dt::sc_bv<N * 4> tmp_data_in = data_in;  // Ensure data_in is properly connected
+  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid;  // Ensure data_in_valid is properly connected
+
   for (int i = 0; i < actual_length; i++)
   {
     tmp_data = *(data + (i/2));
@@ -16,72 +20,110 @@ void packetGenerator::fill_data(unsigned char* data, int nibbles_length)
     tmp_data_in.range(i * 4 + 3, i * 4) = tmp_data;
     tmp_data_valid[i] = 1;
   }
-  data_in.write(tmp_data_in);
-  data_in_valid.write(tmp_data_valid);
-  next_data_in_valid.write(tmp_data_valid);
+
+  data_in = tmp_data_in;
+  data_in_valid = tmp_data_valid;
+  n1_data_in_valid = tmp_data_valid;
+  n2_data_in_valid = tmp_data_valid;
+}
+
+void packetGenerator::processing()
+{
+  generate_packet();
 }
 
 void packetGenerator::generate_packet()
 {
-  sc_dt::sc_bv<N * 4> tmp_data_in = data_in.read();
-  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid.read();
-  
+  sc_dt::sc_bv<N * 4> tmp_data_in = data_in;
+  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid;
+
   bool manual_update = false;
-  
+
+  n1_data_out = n2_data_out;
+  n1_data_out_valid = n2_data_out_valid;
+  n1_data_in_valid = n2_data_in_valid;
+
   if (tmp_data_valid.or_reduce())
   {
     for (int i = 0; i < N; i++)
     {
       if (tmp_data_valid[i] == 1)
       {
-        if ((bitCount == 0) && (next_data_out_valid.read() == 0))
+        if ((bitCount == 0) && (tmp_data_out_valid == false) && (n1_data_out_valid == false))
         {
           tmp_data_valid[i] = 0;
-          next_data_out.write(tmp_data_in.range(i * 4 + 3, i * 4));
-          next_data_out_valid.write(1);
-          next_data_in_valid.write(tmp_data_valid);
+          n2_data_out = tmp_data_in.range(i * 4 + 3, i * 4);
+          n2_data_out_valid = true;
+          n2_data_in_valid = tmp_data_valid;
+          std::cout << "@" << sc_time_stamp() << " Inside generate_packet(): data to sent " << n2_data_out << std::endl;
           break;
         }
-        else if (bitCount == 4)
+        else if ((bitCount == 0) && (tmp_data_out_valid == true))
         {
           tmp_data_valid[i] = 0;
           data_out.write(tmp_data_in.range(i * 4 + 3, i * 4));
           data_out_valid.write(1);
-          data_in_valid.write(tmp_data_valid);
-          next_data_out.write(tmp_data_in.range(i * 4 + 3, i * 4));
-          next_data_out_valid.write(1);
-          next_data_in_valid.write(tmp_data_valid);
+          tmp_data_out_valid = true;
+          data_in_valid = tmp_data_valid;
+          n2_data_out = tmp_data_in.range(i * 4 + 3, i * 4);
+          n2_data_out_valid = true;
+          n2_data_in_valid = tmp_data_valid;
+          n1_data_out = n2_data_out;
+          n1_data_out_valid = n2_data_out_valid;
+          n1_data_in_valid = n2_data_in_valid;
           manual_update = true;
+          std::cout << "@" << sc_time_stamp() << " Inside generate_packet(): data to sent " << n2_data_out << std::endl;
           break;
         }
       }
     }
   }
 
-  if (data_out_valid.read() == 1)
+  if (!manual_update)
+  {
+    data_out_valid.write(n1_data_out_valid);
+    tmp_data_out_valid = n1_data_out_valid;
+  }
+
+  if (tmp_data_out_valid == true)
   {
     bitCount++;
   }
 
-  if(bitCount == 5)
+  if (bitCount == 5)
   {
     bitCount = 0;
-    if (manual_update == false)
+    if (!manual_update)
     {
-      next_data_out_valid.write(0);
-      data_out_valid.write(0);
+      n2_data_out_valid = false;
+      n1_data_out_valid = n2_data_out_valid;
+      data_out_valid.write(true);
+      tmp_data_out_valid = true;
     }
   }
-  else if (manual_update == false)
+
+  if (!manual_update)
   {
-    data_out_valid.write(next_data_out_valid.read());
+    data_in_valid = n1_data_in_valid;
+    data_out.write(n1_data_out);
   }
 
-  if (manual_update == false)
-  {
-    data_in_valid.write(next_data_in_valid.read());
-    data_out.write(next_data_out.read());
-  }
+  n1_sigBitCount = n2_sigBitCount;
+  n2_sigBitCount = bitCount;
+  n1_sigBitCount_.write(n1_sigBitCount);
+  n2_sigBitCount_.write(n2_sigBitCount);
+  sigBitCount.write(n1_sigBitCount);
 
-  sigBitCount.write(bitCount);
+  tmp_data_out_valid_.write(tmp_data_out_valid);
+
+  n2_data_out_valid_.write(n2_data_out_valid);
+  n2_data_out_.write(n2_data_out);
+  n2_data_in_valid_.write(n2_data_in_valid);
+
+  n1_data_out_valid_.write(n1_data_out_valid);
+  n1_data_out_.write(n1_data_out);
+  n1_data_in_valid_.write(n1_data_in_valid);
+
+  data_in_.write(data_in);
+  data_in_valid_.write(data_in_valid);
 }
--- modules/ethernetAMS/src/tb_ethernet_encoder.cpp
+++ modules/ethernetAMS/src/tb_ethernet_encoder.cpp
@@ -5,52 +5,83 @@
 
 int sc_main(int argc, char* argv[])
 {
-    sca_tdf::sca_signal<sc_dt::sc_bv<4>> data_in_signal;
+    sca_tdf::sca_signal<bool> valid;
     sca_tdf::sca_signal<double> mlt3_out_signal;
 
-    sc_signal<bool> cclk;
-    sc_signal<bool> data_out_valid;
-    sc_signal<sc_dt::sc_bv<4>> data_out;
+    sca_tdf::sca_signal<bool> data_out_valid;
+    sca_tdf::sca_signal<sc_dt::sc_bv<4>> data_out;
+
+    sca_tdf::sca_signal<bool> tmp_data_out_valid;
+
+    sca_tdf::sca_signal<bool> n2_data_out_valid;
+    sca_tdf::sca_signal<sc_dt::sc_bv<4>> n2_data_out;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n2_data_in_valid;
+    sca_tdf::sca_signal<bool> n1_data_out_valid;
+    sca_tdf::sca_signal<sc_dt::sc_bv<4>> n1_data_out;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n1_data_in_valid;
+
+    sca_tdf::sca_signal<sc_dt::sc_bv<64>> data_in;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> data_in_valid;
+    sca_tdf::sca_signal<sc_dt::sc_int<4>> n1_sigBitCount;
+    sca_tdf::sca_signal<sc_dt::sc_int<4>> n2_sigBitCount;
+    sca_tdf::sca_signal<sc_dt::sc_int<4>> sigBitCount;
+
 
     unsigned char data[8];
 
-    InputSignalGenerator inputGen("inputGen");
     ethernetEncoder encoder("encoder");
-    clock_generator clk_gen("clk_gen");
     packetGenerator pkt_gen("pkt_gen");
 
-    inputGen.out(data_in_signal);
-    encoder.data_in(data_in_signal);
+    encoder.data_in(data_out);
     encoder.mlt3_out(mlt3_out_signal);
+    encoder.valid(data_out_valid);
 
-    clk_gen.clk_out(cclk);
-    pkt_gen.cclk(cclk);
     pkt_gen.data_out_valid(data_out_valid);
     pkt_gen.data_out(data_out);
+
+    pkt_gen.tmp_data_out_valid_(tmp_data_out_valid);
+
+    pkt_gen.n2_data_out_valid_(n2_data_out_valid);
+    pkt_gen.n2_data_out_(n2_data_out);
+    pkt_gen.n2_data_in_valid_(n2_data_in_valid);
+
+    pkt_gen.n1_data_out_valid_(n1_data_out_valid);
+    pkt_gen.n1_data_out_(n1_data_out);
+    pkt_gen.n1_data_in_valid_(n1_data_in_valid);
+
+    pkt_gen.data_in_(data_in);
+    pkt_gen.data_in_valid_(data_in_valid);
+    pkt_gen.n1_sigBitCount_(n1_sigBitCount);
+    pkt_gen.n2_sigBitCount_(n2_sigBitCount);
+    pkt_gen.sigBitCount(sigBitCount);
 
     // Trace file setup
     sca_util::sca_trace_file *traceFile = sca_util::sca_create_vcd_trace_file("ethernetEncoder");
 
     if (traceFile)
     {
-        sca_util::sca_trace(traceFile, data_in_signal, "data_in");
         sca_util::sca_trace(traceFile, mlt3_out_signal, "mlt3_out");
-        sca_util::sca_trace(traceFile, cclk, "cclk");
         sca_util::sca_trace(traceFile, data_out_valid, "data_out_valid");
         sca_util::sca_trace(traceFile, data_out, "data_out");
-        sca_util::sca_trace(traceFile, pkt_gen.data_in, "pkt_gen_data_in");
-        sca_util::sca_trace(traceFile, pkt_gen.data_in_valid, "pkt_gen_data_in_valid");
-        sca_util::sca_trace(traceFile, pkt_gen.next_data_in_valid, "pkt_gen_next_data_in_valid");
-        sca_util::sca_trace(traceFile, pkt_gen.next_data_out_valid, "pkt_gen_next_data_out_valid");
-        sca_util::sca_trace(traceFile, pkt_gen.next_data_out, "pkt_gen_next_data_out");
-        sca_util::sca_trace(traceFile, pkt_gen.sigBitCount, "pkt_gen_sigBitCount");
+        sca_util::sca_trace(traceFile, data_in, "pkt_gen_data_in");
+        sca_util::sca_trace(traceFile, data_in_valid, "pkt_gen_data_in_valid");
+        sca_util::sca_trace(traceFile, n2_data_in_valid, "pkt_gen_n2_data_in_valid");
+        sca_util::sca_trace(traceFile, n2_data_out_valid, "pkt_gen_n2_data_out_valid");
+        sca_util::sca_trace(traceFile, n2_data_out, "pkt_gen_n2_data_out");
+        sca_util::sca_trace(traceFile, n1_data_in_valid, "pkt_gen_n1_data_in_valid");
+        sca_util::sca_trace(traceFile, n1_data_out_valid, "pkt_gen_n1_data_out_valid");
+        sca_util::sca_trace(traceFile, n1_data_out, "pkt_gen_n1_data_out");
+        sca_util::sca_trace(traceFile, sigBitCount, "pkt_gen_sigBitCount");
+        sca_util::sca_trace(traceFile, n1_sigBitCount, "pkt_gen_n1_sigBitCount");
+        sca_util::sca_trace(traceFile, n2_sigBitCount, "pkt_gen_n2_sigBitCount");
+        sca_util::sca_trace(traceFile, tmp_data_out_valid, "pkt_gen_tmp_data_out_valid");
     }
 
     sc_start(45, SC_NS);
     data[0] = 113;
     data[1] = 255;
     data[2] = 204;
-    pkt_gen.fill_data(data, 5);
+    pkt_gen.fill_data(data, 3);
 
     sc_start(455, SC_NS); // Run the simulation for 500 ns
 
