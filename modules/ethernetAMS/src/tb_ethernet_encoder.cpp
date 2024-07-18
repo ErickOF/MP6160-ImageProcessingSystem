ec61e69846fee05e4a378a736e0b49b0269f4789
fa-jimenez <najimenezch@gmail.com>
2024-07-17 19:12:24 -0600

    Connect encoder with packet generator and change the generator to include preamble of ethernet packets

--- modules/ethernetAMS/src/tb_ethernet_encoder.cpp
+++ modules/ethernetAMS/src/tb_ethernet_encoder.cpp
@@ -15,13 +15,17 @@ int sc_main(int argc, char* argv[])
 
     sca_tdf::sca_signal<bool> n2_data_out_valid;
     sca_tdf::sca_signal<sc_dt::sc_bv<4>> n2_data_out;
-    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n2_data_in_valid;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n2_data_valid;
     sca_tdf::sca_signal<bool> n1_data_out_valid;
     sca_tdf::sca_signal<sc_dt::sc_bv<4>> n1_data_out;
-    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n1_data_in_valid;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> n1_data_valid;
 
     sca_tdf::sca_signal<sc_dt::sc_bv<64>> data_in;
     sca_tdf::sca_signal<sc_dt::sc_bv<16>> data_in_valid;
+
+    sca_tdf::sca_signal<sc_dt::sc_bv<64>> data_to_send;
+    sca_tdf::sca_signal<sc_dt::sc_bv<16>> data_valid_to_send;
+
     sca_tdf::sca_signal<sc_dt::sc_int<4>> n1_sigBitCount;
     sca_tdf::sca_signal<sc_dt::sc_int<4>> n2_sigBitCount;
     sca_tdf::sca_signal<sc_dt::sc_int<4>> sigBitCount;
@@ -43,14 +47,18 @@ int sc_main(int argc, char* argv[])
 
     pkt_gen.n2_data_out_valid_(n2_data_out_valid);
     pkt_gen.n2_data_out_(n2_data_out);
-    pkt_gen.n2_data_in_valid_(n2_data_in_valid);
+    pkt_gen.n2_data_valid_(n2_data_valid);
 
     pkt_gen.n1_data_out_valid_(n1_data_out_valid);
     pkt_gen.n1_data_out_(n1_data_out);
-    pkt_gen.n1_data_in_valid_(n1_data_in_valid);
+    pkt_gen.n1_data_valid_(n1_data_valid);
 
     pkt_gen.data_in_(data_in);
     pkt_gen.data_in_valid_(data_in_valid);
+
+    pkt_gen.data_to_send_(data_to_send);
+    pkt_gen.data_valid_to_send_(data_valid_to_send);
+
     pkt_gen.n1_sigBitCount_(n1_sigBitCount);
     pkt_gen.n2_sigBitCount_(n2_sigBitCount);
     pkt_gen.sigBitCount(sigBitCount);
@@ -65,16 +73,18 @@ int sc_main(int argc, char* argv[])
         sca_util::sca_trace(traceFile, data_out, "data_out");
         sca_util::sca_trace(traceFile, data_in, "pkt_gen_data_in");
         sca_util::sca_trace(traceFile, data_in_valid, "pkt_gen_data_in_valid");
-        sca_util::sca_trace(traceFile, n2_data_in_valid, "pkt_gen_n2_data_in_valid");
+        sca_util::sca_trace(traceFile, n2_data_valid, "pkt_gen_n2_data_valid");
         sca_util::sca_trace(traceFile, n2_data_out_valid, "pkt_gen_n2_data_out_valid");
         sca_util::sca_trace(traceFile, n2_data_out, "pkt_gen_n2_data_out");
-        sca_util::sca_trace(traceFile, n1_data_in_valid, "pkt_gen_n1_data_in_valid");
+        sca_util::sca_trace(traceFile, n1_data_valid, "pkt_gen_n1_data_valid");
         sca_util::sca_trace(traceFile, n1_data_out_valid, "pkt_gen_n1_data_out_valid");
         sca_util::sca_trace(traceFile, n1_data_out, "pkt_gen_n1_data_out");
         sca_util::sca_trace(traceFile, sigBitCount, "pkt_gen_sigBitCount");
         sca_util::sca_trace(traceFile, n1_sigBitCount, "pkt_gen_n1_sigBitCount");
         sca_util::sca_trace(traceFile, n2_sigBitCount, "pkt_gen_n2_sigBitCount");
         sca_util::sca_trace(traceFile, tmp_data_out_valid, "pkt_gen_tmp_data_out_valid");
+        sca_util::sca_trace(traceFile, data_to_send, "pkt_gen_data_to_send");
+        sca_util::sca_trace(traceFile, data_valid_to_send, "pkt_gen_data_valid_to_send");
     }
 
     sc_start(45, SC_NS);
@@ -83,7 +93,7 @@ int sc_main(int argc, char* argv[])
     data[2] = 204;
     pkt_gen.fill_data(data, 3);
 
-    sc_start(455, SC_NS); // Run the simulation for 500 ns
+    sc_start(1455, SC_NS); // Run the simulation for 500 ns
 
     if (traceFile)
     {
