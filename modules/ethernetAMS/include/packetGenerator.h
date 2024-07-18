6462ca3a6173c83b3c39e859931a59cd6dc319f5
fa-jimenez <najimenezch@gmail.com>
2024-07-17 19:12:24 -0600

    Connect encoder with packet generator and change the generator to include preamble of ethernet packets

--- modules/ethernetAMS/include/packetGenerator.h
+++ modules/ethernetAMS/include/packetGenerator.h
@@ -10,49 +10,65 @@ SCA_TDF_MODULE(packetGenerator)
     n1_data_out_valid = false;
     n2_data_out_valid = false;
     n2_sigBitCount = 0;
+
+    preamble_in_process = false;
+    data_length_in_process = false;
   }
 
   #define N 16
 
+  // Main results from block
   sca_tdf::sca_out<bool> data_out_valid;
   sca_tdf::sca_out<sc_dt::sc_bv<4>> data_out;
 
+  // Signals for tracing the values
   sca_tdf::sca_out<bool> tmp_data_out_valid_;
 
   sca_tdf::sca_out<bool> n1_data_out_valid_;
   sca_tdf::sca_out<sc_dt::sc_bv<4>> n1_data_out_;
-  sca_tdf::sca_out<sc_dt::sc_bv<N>> n1_data_in_valid_;
+  sca_tdf::sca_out<sc_dt::sc_bv<N>> n1_data_valid_;
 
   sca_tdf::sca_out<bool> n2_data_out_valid_;
   sca_tdf::sca_out<sc_dt::sc_bv<4>> n2_data_out_;
-  sca_tdf::sca_out<sc_dt::sc_bv<N>> n2_data_in_valid_;
+  sca_tdf::sca_out<sc_dt::sc_bv<N>> n2_data_valid_;
 
   sca_tdf::sca_out<sc_dt::sc_bv<N * 4>> data_in_;
   sca_tdf::sca_out<sc_dt::sc_bv<N>> data_in_valid_;
+
+  sca_tdf::sca_out<sc_dt::sc_bv<N * 4>> data_to_send_;
+  sca_tdf::sca_out<sc_dt::sc_bv<N>> data_valid_to_send_;
+
   sca_tdf::sca_out<sc_dt::sc_int<4>> n1_sigBitCount_;
   sca_tdf::sca_out<sc_dt::sc_int<4>> n2_sigBitCount_;
   sca_tdf::sca_out<sc_dt::sc_int<4>> sigBitCount;
 
+  // Internal values to do the logic
   bool tmp_data_out_valid;
 
   bool n1_data_out_valid;
   sc_dt::sc_bv<4> n1_data_out;
-  sc_dt::sc_bv<N> n1_data_in_valid;
+  sc_dt::sc_bv<N> n1_data_valid;
   sc_dt::sc_int<4> n1_sigBitCount;
 
   bool n2_data_out_valid;
   sc_dt::sc_bv<4> n2_data_out;
-  sc_dt::sc_bv<N> n2_data_in_valid;
+  sc_dt::sc_bv<N> n2_data_valid;
   sc_dt::sc_int<4> n2_sigBitCount;
 
+  bool preamble_in_process;
+  bool data_length_in_process;
+
+  // Data to be sent, set in fill_data()
   sc_dt::sc_bv<N * 4> data_in;
   sc_dt::sc_bv<N> data_in_valid;
+
+  sc_dt::sc_bv<N * 4> data_to_send;
+  sc_dt::sc_bv<N> data_valid_to_send;
 
   int bitCount;
 
   void fill_data(unsigned char* data, int data_length);
-  void generate_packet();
+
   void processing();
 
-  // Removed sc_in<bool> cclk;
 };
--- modules/ethernetAMS/src/packetGenerator.cpp
+++ modules/ethernetAMS/src/packetGenerator.cpp
@@ -2,13 +2,12 @@
 #include <systemc.h>
 #include "packetGenerator.h"
 
-
 void packetGenerator::fill_data(unsigned char* data, int packet_length)
 {
   int actual_length = (packet_length * 2 > N) ? N : packet_length * 2;
   unsigned char tmp_data;
-  sc_dt::sc_bv<N * 4> tmp_data_in = data_in;  // Ensure data_in is properly connected
-  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid;  // Ensure data_in_valid is properly connected
+  sc_dt::sc_bv<N * 4> tmp_data_in = 0;
+  sc_dt::sc_bv<N> tmp_data_valid = 0;
 
   for (int i = 0; i < actual_length; i++)
   {
@@ -23,54 +22,100 @@ void packetGenerator::fill_data(unsigned char* data, int packet_length)
 
   data_in = tmp_data_in;
   data_in_valid = tmp_data_valid;
-  n1_data_in_valid = tmp_data_valid;
-  n2_data_in_valid = tmp_data_valid;
+
+  // Insert first the preamble
+  for (int i = 0; i < 15; i++)
+  {
+    std::cout << "@" << sc_time_stamp() << " Inside fill_data(): i " << i << std::endl;
+    data_to_send.range(i * 4 + 3, i * 4) = "1010";
+  }
+  data_to_send.range(63, 60) = "1011";
+  data_valid_to_send = "1111111111111111";
+
+  n2_data_valid = data_valid_to_send;
+  n1_data_valid = n2_data_valid;
+
+  preamble_in_process = true;
 }
 
 void packetGenerator::processing()
 {
-  generate_packet();
-}
-
-void packetGenerator::generate_packet()
-{
-  sc_dt::sc_bv<N * 4> tmp_data_in = data_in;
-  sc_dt::sc_bv<N> tmp_data_valid = data_in_valid;
+  sc_dt::sc_bv<N * 4> tmp_data_to_send = data_to_send;
+  sc_dt::sc_bv<N> tmp_data_valid_to_send = data_valid_to_send;
 
   bool manual_update = false;
 
+  if ((tmp_data_valid_to_send.or_reduce() == 0) && (preamble_in_process == true))
+  {
+    unsigned char length = 0;
+    sc_dt::sc_bv<8> local_length = 0;
+
+    preamble_in_process = false;
+    data_length_in_process = true;
+
+    for (int i = 0; i < N; i++)
+    {
+      if (data_in_valid[i] == 1)
+      {
+        length++;
+      }
+    }
+    local_length = length / 2;
+
+    data_to_send = 0;
+    data_to_send.range(7, 0) = local_length;
+    data_valid_to_send = "11";
+    tmp_data_to_send = data_to_send;
+    tmp_data_valid_to_send = data_valid_to_send;
+
+    n2_data_valid = data_valid_to_send;
+    n1_data_valid = n2_data_valid;
+  }
+  else if ((tmp_data_valid_to_send.or_reduce() == 0) && (data_length_in_process == true))
+  {
+    data_length_in_process = false;
+
+    data_to_send = data_in;
+    data_valid_to_send = data_in_valid;
+    tmp_data_to_send = data_to_send;
+    tmp_data_valid_to_send = data_valid_to_send;
+
+    n2_data_valid = data_valid_to_send;
+    n1_data_valid = n2_data_valid;
+  }
+
   n1_data_out = n2_data_out;
   n1_data_out_valid = n2_data_out_valid;
-  n1_data_in_valid = n2_data_in_valid;
+  n1_data_valid = n2_data_valid;
 
-  if (tmp_data_valid.or_reduce())
+  if (tmp_data_valid_to_send.or_reduce())
   {
     for (int i = 0; i < N; i++)
     {
-      if (tmp_data_valid[i] == 1)
+      if (tmp_data_valid_to_send[i] == 1)
       {
         if ((bitCount == 0) && (tmp_data_out_valid == false) && (n1_data_out_valid == false))
         {
-          tmp_data_valid[i] = 0;
-          n2_data_out = tmp_data_in.range(i * 4 + 3, i * 4);
+          tmp_data_valid_to_send[i] = 0;
+          n2_data_out = tmp_data_to_send.range(i * 4 + 3, i * 4);
           n2_data_out_valid = true;
-          n2_data_in_valid = tmp_data_valid;
+          n2_data_valid = tmp_data_valid_to_send;
           std::cout << "@" << sc_time_stamp() << " Inside generate_packet(): data to sent " << n2_data_out << std::endl;
           break;
         }
         else if ((bitCount == 0) && (tmp_data_out_valid == true))
         {
-          tmp_data_valid[i] = 0;
-          data_out.write(tmp_data_in.range(i * 4 + 3, i * 4));
+          tmp_data_valid_to_send[i] = 0;
+          data_out.write(tmp_data_to_send.range(i * 4 + 3, i * 4));
           data_out_valid.write(1);
           tmp_data_out_valid = true;
-          data_in_valid = tmp_data_valid;
-          n2_data_out = tmp_data_in.range(i * 4 + 3, i * 4);
+          data_valid_to_send_ = tmp_data_valid_to_send;
+          n2_data_out = tmp_data_to_send.range(i * 4 + 3, i * 4);
           n2_data_out_valid = true;
-          n2_data_in_valid = tmp_data_valid;
+          n2_data_valid = tmp_data_valid_to_send;
           n1_data_out = n2_data_out;
           n1_data_out_valid = n2_data_out_valid;
-          n1_data_in_valid = n2_data_in_valid;
+          n1_data_valid = n2_data_valid;
           manual_update = true;
           std::cout << "@" << sc_time_stamp() << " Inside generate_packet(): data to sent " << n2_data_out << std::endl;
           break;
@@ -104,7 +149,7 @@ void packetGenerator::generate_packet()
 
   if (!manual_update)
   {
-    data_in_valid = n1_data_in_valid;
+    data_valid_to_send = n1_data_valid;
     data_out.write(n1_data_out);
   }
 
@@ -118,12 +163,15 @@ void packetGenerator::generate_packet()
 
   n2_data_out_valid_.write(n2_data_out_valid);
   n2_data_out_.write(n2_data_out);
-  n2_data_in_valid_.write(n2_data_in_valid);
+  n2_data_valid_.write(n2_data_valid);
 
   n1_data_out_valid_.write(n1_data_out_valid);
   n1_data_out_.write(n1_data_out);
-  n1_data_in_valid_.write(n1_data_in_valid);
+  n1_data_valid_.write(n1_data_valid);
 
   data_in_.write(data_in);
   data_in_valid_.write(data_in_valid);
+
+  data_to_send_.write(data_to_send);
+  data_valid_to_send_.write(data_valid_to_send);
 }
